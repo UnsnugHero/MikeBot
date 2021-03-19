85d39a1b4749008d86ac19547affc2a53c29c632
Kurt Snuggly <unsnugHero@gmail.com>
2021-03-18 23:16:42 -0400

    setup a new index to listen for events

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
 // Entry point for all bots
+import Discord, { Message } from 'discord.js';
+
+import 'dotenv/config';
+
+// discord
+const client = new Discord.Client();
+const token = process.env.DISCORD_BOT_TOKEN;
+
+/*****************************
+ * DISCORD
+ ****************************/
+
+// event listeners
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// listen for messages in channels
+client.on('message', (msg: Message) => {
+  // assuming content can;t be blank or undefined
+  const { content } = msg;
+
+  msg.reply(content);
+});
+
+client.login(token);
