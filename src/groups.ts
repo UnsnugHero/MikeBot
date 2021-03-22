048252804c363030176a5e1c3dbceb7857a811af
Kurt Snuggly <unsnugHero@gmail.com>
2021-03-21 20:07:40 -0400

    remove package and optimize groups

--- src/groups.ts
+++ src/groups.ts
@@ -1,29 +1,38 @@
 // File for declaring and managing command groups for each module of the bot.
-// Inclusion of a group will be determined by a config file corresponding to
-// CommandType ids.
+// Inclusion of a group will be determined by a config boolean
 import { CommandGroup, CommandoClient } from 'discord.js-commando';
 
 // custom type for command ids. Each type corresponds to a bot module.
 type CommandType = 'dice' | 'dogs' | 'todo';
 
+interface GroupConfig {
+  id: CommandType;
+  name: string;
+  included: boolean;
+}
+
+const groupConfigs: GroupConfig[] = [
+  { id: 'dice', name: 'Dice Commands', included: true },
+  { id: 'dogs', name: 'Dogs Commands', included: false },
+  { id: 'todo', name: 'Todo Commands', included: false },
+];
+
+const groupMaker = (
+  client: CommandoClient,
+  id: CommandType,
+  name: string
+): CommandGroup => {
+  return new CommandGroup(client, id, name);
+};
+
 export const GROUPS = (client: CommandoClient): CommandGroup[] => {
-  return [DICE_GROUP(client), DOGS_GROUP(client), TODO_GROUP(client)];
-};
+  const groups = [];
 
-const DICE_GROUP = (client: CommandoClient): CommandGroup => {
-  const DICE_GROUP_ID: CommandType = 'dice';
-  const DICE_GROUP_NAME = 'Dice Commands';
-  return new CommandGroup(client, DICE_GROUP_ID, DICE_GROUP_NAME);
-};
+  groupConfigs.forEach((groupConfig) => {
+    if (groupConfig.included) {
+      groups.push(groupMaker(client, groupConfig.id, groupConfig.name));
+    }
+  });
 
-const DOGS_GROUP = (client: CommandoClient): CommandGroup => {
-  const DOGS_GROUP_ID: CommandType = 'dogs';
-  const DOGS_GROUP_NAME = 'Dogs Commands';
-  return new CommandGroup(client, DOGS_GROUP_ID, DOGS_GROUP_NAME);
-};
-
-const TODO_GROUP = (client: CommandoClient): CommandGroup => {
-  const TODO_GROUP_ID: CommandType = 'todo';
-  const TODO_GROUP_NAME = 'Todo Commands';
-  return new CommandGroup(client, TODO_GROUP_ID, TODO_GROUP_NAME);
+  return groups;
 };
